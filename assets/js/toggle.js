3c689cdedc437f988ede195030dd28e1c7e10fc0
shayan-dev <shayankamandi82@gmail.com>
2021-02-01 13:23:59 +0330

    hey github

---
+++ assets/js/toggle.js
@@ -0,0 +1,18 @@
+// toggle mobile menu
+
+// const toggle = document.querySelector('.navbar-toggle');
+// const menu = document.querySelector('.navbar-nav');
+
+// function togglemenu() {
+//     if (menu.classList.contains("nav-item")) {
+//         menu.classList.remove("nav-item");
+//         toggle.querySelector("a").innerHTML = "<i class='fa fa-bars'></i>";
+//     } else {
+//         menu.classList.add("active");
+//         toggle.querySelector("a").innerHTML = "<i class='fa fa-bars'></i>";
+        
+//     }
+// }
+
+// toggle.addEventListener('click', togglemenu, false);}
+
