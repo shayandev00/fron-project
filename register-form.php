577f7d1dc27466ecc655df8f6e6caa77521b231b
shayandev00 <shayankamandi82@gmail.com>
2021-02-07 16:53:06 +0330

    Hello

--- register-form.php
+++ register-form.php
@@ -22,37 +22,39 @@
 // }
 //
 
-//VALUE of my input
-    $user = $_POST('user');
-    $email = $_POST('email');
-    $txt = $_POST('message');
-//Request to server. Localhost
+$_SERVER['REQUEST_METHOD']  = "POST";
 
-// * $_REQUEST = new \http\Env\Request('Post', 'localhost');
+    $user = $_POST['name'];
+    $email = $_POST['email'];
+    $txt = $_POST['message'];
+    $sub = $_POST['sub'];
 
-//Header for location in post or sth
-
-//  *  header("location: localhost/register-form.php");
-    $url = 'http://localhost:8080/register-form.php';
+    $url = 'http://localhost:8080/front/register-form.php';
     $curl = curl_init($url);
-//     json
 
-$json_data = array(
-    'user' => $user,
-    'email' => $email,
-    'txt' => $txt
-);
-$jsonDataEncode = json_encode($json_data);
+//  json
+    $json_data = array(
+        'user' => $user,
+        'email' => $email,
+        'txt' => $txt
+    );
+//    $jsonDataEncode = json_encode($json_data);
 
-curl_setopt($curl,CURLOPT_POST,true);
+    curl_setopt($curl,CURLOPT_POST,true);
 
-$server_output = curl_exec($curl);
+    $server_output = curl_exec($curl);
 
-curl_close($curl);
+    curl_close($curl);
 
-if ($server_output == "OK"){
-    echo "hey";
-}else{
-    echo "Error Shayan"; 
-}
-?>
+    for($i = 0; $i = count($json_data); $i++){
+        if(isset($_POST['sub'])){
+            echo("User: " . $_POST['name'] . "<br />");
+            echo("Email: " . $_POST['email'] . "<br />");
+            echo("Text: ". $_POST['message'] . "<br />");
+        }
+        else{
+            echo "Error Shayan";
+          }
+    break;
+    }
+
