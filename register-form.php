85b88fa03c7fc1e4aa6ee6a73574416210e320f7
shayandev00 <shayankamandi82@gmail.com>
2021-02-11 14:06:37 +0330

    improve

--- register-form.php
+++ register-form.php
@@ -24,37 +24,28 @@
 
 $_SERVER['REQUEST_METHOD']  = "POST";
 
-    $user = $_POST['name'];
-    $email = $_POST['email'];
-    $txt = $_POST['message'];
-    $sub = $_POST['sub'];
+$user = $_POST['name'];
+$email = $_POST['email'];
+$txt = $_POST['message'];
+$sub = $_POST['sub'];
 
-    $url = 'http://localhost:8080/front/register-form.php';
-    $curl = curl_init($url);
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $db = "test2";
 
-//  json
-    $json_data = array(
-        'user' => $user,
-        'email' => $email,
-        'txt' => $txt
-    );
-//    $jsonDataEncode = json_encode($json_data);
+try {
+        $conn = new PDO("mysql:host=$servername; dbname=$db", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $datas = $conn->prepare("INSERT INTO testcolumn(user , email, message) VALUES ('$user', '$email','$txt')");
 
-    curl_setopt($curl,CURLOPT_POST,true);
-
-    $server_output = curl_exec($curl);
-
-    curl_close($curl);
-
-    for($i = 0; $i = count($json_data); $i++){
-        if(isset($_POST['sub'])){
-            echo("User: " . $_POST['name'] . "<br />");
-            echo("Email: " . $_POST['email'] . "<br />");
-            echo("Text: ". $_POST['message'] . "<br />");
-        }
-        else{
-            echo "Error Shayan";
-          }
-    break;
+    $datas->execute();
+        echo "Connected SuccessFully";
     }
+    catch(PDOException $e){
+    echo "Connection Failed: " . $e->getMessage();
+}
+
+?>
+
 
---
+++ show-data.php
@@ -0,0 +1,44 @@
+<?php
+
+$_SERVER['REQUEST_METHOD']  = "POST";
+
+//$user = $_POST['name'];
+//$email = $_POST['email'];
+//$txt = $_POST['message'];
+//$sub = $_POST['sub'];
+//
+//    $url = 'http://localhost:8080/front/register-form.php';
+//    $curl = curl_init($url);
+//
+////  json
+//    $json_data = array(
+//        'user' => $user,
+//       'email' => $email,
+//        'txt' => $txt
+//    );
+//
+//    $jsonDataEncode = json_encode($json_data);
+//
+//    curl_setopt($curl,CURLOPT_POST,true);
+//
+//    $server_output = curl_exec($curl);
+//
+//    curl_close($curl);
+//
+//    for($i = 0; $i = count($json_data); $i++){
+//        if(isset($_POST['sub'])){
+//            echo("User: " . $_POST['name'] . "<br />");
+//            echo("Email: " . $_POST['email'] . "<br />");
+//            echo("Text: ". $_POST['message'] . "<br />");
+//
+
+//        }
+//        else{
+//            $x =1;
+//            while ($x <= 5){
+//                echo "this number is: $x";
+//                $x++;
+//            }
+//            break;
+//        }
+//    }
