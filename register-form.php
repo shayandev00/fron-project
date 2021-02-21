bd2625b1bed4d5fc7c056835e55a28911200d993
shayandev <shayankamandi82@gmail.com>
2021-02-21 14:26:21 +0330

    test 1.1

--- register-form.php
+++ register-form.php
@@ -33,7 +33,7 @@ $sub = $_POST['sub'];
     $username = "root";
     $password = "";
     $db = "test2";
-
+    echo 'reza';
 try {
         $conn = new PDO("mysql:host=$servername; dbname=$db", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
